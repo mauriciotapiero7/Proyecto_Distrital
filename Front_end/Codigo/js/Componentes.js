411392c63531652dadf29fdf6a12a8ebcdbd4794
mauriciotapiero7 <tavo112011@hotmail.com>
2023-05-22 19:42:48 -0500

    add

--- Front_end/Codigo/js/Componentes.js
+++ Front_end/Codigo/js/Componentes.js
@@ -1,33 +1,32 @@
 class headermain extends HTMLElement{
     constructor(){
-        super();
+    super();
     }
-
+    
     connectedCallback(){
-        this.innerHTML = 
-        <nav>
-            <ul>
-                <li>
-                    <a href="Index.html">INICIO</a>
-                </li>
-                <li>
-                    <a href="Quienes_somos.html">QUIENES SOMOS</a>
-                </li>
-                <li>
-                    <a href="Especialidad.html">ESPECIALIDAD</a>
-                </li>
-                <li>
-                    <a href="Servicios.html">SERVICIOS</a>
-                </li>
-                <li>
-                    <a href="Experiencias.html">EXPERIENCIAS Y CLIENTES</a>
-                </li>
-                <li>
-                    <a href="Contacto.html">CONTACTO</a>
-                </li>
-            </ul>
-        </nav>
-        
+    this.innerHTML = `
+    <ul>
+    <li>
+    <a href="">INICIO</a>
+    </li>
+    <li>
+    <a href="Quienes_somos.html">QUIENES SOMOS</a>
+    </li>
+    <li>
+    <a href="">ESPECIALIDAD</a>
+    </li>
+    <li>
+    <a href="">SERVICIOS</a>
+    </li>
+    <li>
+    <a href="">EXPERIENCIAS Y CLIENTES</a>
+    </li>
+    <li>
+    <a href="">CONTACTO</a>
+    </li>
+    </ul>
+    `;
     }
-}
-window.customElements.define("menu_nav", headermain);
+    }
+    
+    window.customElements.define("menu-main", headermain);
