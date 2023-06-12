72092f8375adcb758c462f22c81484699d5c52c6
mauriciotapiero7 <tavo112011@hotmail.com>
2023-06-12 15:54:37 -0500

    add

---
+++ Front_end/Codigo/Back_end/php/form.php
@@ -0,0 +1,20 @@
+<?php
+$nombre = $_POST ['name'];
+$mail = $_POST ['email'];
+$asunto = $_POST ['asunto'];
+$mensaje = $_POST ['mensaje'];
+
+$mensaje = "Este mensaje fue enviado por" . $nombre .   ". \r\n";    
+$mensaje .= "Su Email es: " . $mail . "\r\n";
+$mensaje .= "El asunto es: " . $asunto . "\r\n";
+$mensaje .= "El mensaje es: " . $mensaje . "\r\n";
+
+
+$para = 'J.carlos_publicidad2@hotmail.com';
+$asu = "Este mensaje es enviado desde la web de publimetal";
+
+mail($para, $asu, utf8_decode($mensaje), header);
+
+header('location:/index.php');
+exit;
+?>
