e0452f2f39b31c13af704f8068fdfa456407d2c3
mauriciotapiero7 <tavo112011@hotmail.com>
2023-06-13 06:13:29 -0500

    add

--- Front_end/Codigo/php/form.php
+++ Front_end/Codigo/php/form.php
@@ -1,20 +1,28 @@
 <?php
 
-$nombre = $_POST ['name'];
-$mail = $_POST ['email'];
-$asunto = $_POST ['asunto'];
-$mensaje = $_POST ['mensaje'];
+if (isset($_POST['enviar']))
+{
+    if(!empty($_POST['name'])&&!empty($_POST['email'])&&!empty($_POST['[asunto'])&&!empty($_POST['mensaje'])){
+        $nombre = $_POST ['name'];
+        $mail = $_POST ['email'];
+        $asunto = $_POST ['asunto'];
+        $mensaje = $_POST ['mensaje'];
 
-$mensaje = "Este mensaje fue enviado por" . $nombre .   ". \r\n";    
-$mensaje .= "Su Email es: " . $mail . "\r\n";
-$mensaje .= "El asunto es: " . $asunto . "\r\n";
-$mensaje .= "El mensaje es: " . $mensaje . "\r\n";
+        $mensaje = "Este mensaje fue enviado por" . $nombre .   ". \r\n";    
+        $mensaje .= "Su Email es: " . $mail . "\r\n";
+        $mensaje .= "El asunto es: " . $asunto . "\r\n";
+        $mensaje .= "El mensaje es: " . $mensaje . "\r\n";
 
 
-$para = 'J.carlos_publicidad2@hotmail.com';
-$asu = "Este mensaje es enviado desde la web de publimetal";
+        $para = 'J.carlos_publicidad2@hotmail.com';
+        $asu = "Este mensaje es enviado desde la web de publimetal";
 
-mail($para, $asu, utf8_decode($mensaje), $header);
+        @mail($para, $asu, utf8_decode($mensaje), $header);
 
-header('location: ../correo.html');
+        header('location: ../correo.html');
+        if($mail){
+            echo "<h4>¡Mail enviado exitosamente!</h4>";
+        }
+    }
+}
 ?>
